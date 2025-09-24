daa4c1bb907b74068076bdc4b505dcbecc8c2fe8
ajp-sourceship <ajp2914@gmail.com>
2025-09-23 22:46:08 -0500

    refactor: Remove sample comments and unused NewAppScreen import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-
 import AppNavigator from './AppNavigator';
 import { StatusBar, StyleSheet } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
