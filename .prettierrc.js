56b043ccc357ff469f72a1313a367e96f1be87bf
ajp-sourceship <ajp2914@gmail.com>
2025-11-21 06:17:28 -0600

    fix: remove unnecessary trailing newlines in CircleCI config and improve Prettier configuration for YAML files

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,14 @@ module.exports = {
   arrowParens: 'avoid',
   singleQuote: true,
   trailingComma: 'all',
+  overrides: [
+    {
+      files: ['*.yml', '*.yaml'],
+      options: {
+        tabWidth: 2,
+        printWidth: 80,
+        singleQuote: false,
+      },
+    },
+  ],
 };
