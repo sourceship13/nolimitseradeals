bd3b949db2d56ac150155e931c409902de7791c8
ajp-sourceship <ajp2914@gmail.com>
2025-11-29 02:44:38 -0600

    fix: Remove hardcoded Node.js paths for CircleCI compatibility
    - Changed android/node-wrapper.sh to use 'node' from PATH instead of absolute path
    - Commented out nodeExecutableAndArgs in android/app/build.gradle
    - Fixes CircleCI build failure: 'command /Users/alpha/.nvm/versions/node/v20.19.5/bin/node' not found
    - Allows builds to work in both local (nvm) and CI (system node) environments

--- android/node-wrapper.sh
+++ android/node-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Node.js wrapper for Gradle builds
-export PATH="/Users/alpha/.nvm/versions/node/v20.19.5/bin:/usr/local/bin:$PATH"
-exec /Users/alpha/.nvm/versions/node/v20.19.5/bin/node "$@"
+# Use the node from PATH (works in CI and local environments)
+exec node "$@"
